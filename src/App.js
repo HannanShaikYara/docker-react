69c237891deade8ba1506365f6e6b5f67a95afd0
Hannan <hannan.shaik@yara.com>
2019-02-17 18:19:39 +0800

    Added a new v2 on feature branch

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Hello <code>i am changing</code>
+            Hello <code>i am v2</code>
           </p>
           <a
             className="App-link"
